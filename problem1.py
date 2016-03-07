382e74e900d1cd83d82b19ab99d4237d3a71fadb
vindard <vinda.rd@gmail.com>
2016-03-06 22:18:16 -0400

    Rename variable 'l' to 'list'
    Renamed based on a tip about scaling and best practice

--- problem1.py
+++ problem1.py
@@ -1,14 +1,14 @@
 x = 1 #start at this natural number
-l = [] #clearing set for reruns in Jupyter notebook
+list = [] #clearing set for reruns in Jupyter notebook
 sum = 0 #setting sum variable
 below_limit = 1000 #end at this natural number
 
 while x < below_limit:
     if (x % 3 == 0) or (x % 5 == 0):
-        l.append(x)
+        list.append(x)
         sum += x
     x += 1
     
-print ('Sum: \n{} \n\nNumbers:\n{}'.format(sum,l))
+print ('Sum: \n{} \n\nNumbers:\n{}'.format(sum,list))
 
 raw_input("Press any key to exit...")
