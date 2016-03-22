aad5a8dd4e661c940b1178ca448863cb610fa1e1
vindard <vinda.rd@gmail.com>
2016-03-21 21:16:20 -0400

    Update tic-tac-toe.py
    Condensed the final code into a final function instead and made executable into a one-liner

--- Tic-Tac-Toe-Game/tic-tac-toe.py
+++ Tic-Tac-Toe-Game/tic-tac-toe.py
@@ -49,6 +49,7 @@ def win_check():
 #function to get input for next move
 def make_move():
     options = ['1','2','3','4','5','6','7','8','9']
+    move = ""
     
     #choose symbol
     if play_count % 2 == 1: 
@@ -59,46 +60,55 @@ def make_move():
         symbol = 'O'
     
     #take move    
-    move = raw_input("{}'s ({}) move: ".format(current_player,symbol))
     while ((move in entered) or (move not in options)):  #checks for valid input
         print "Please enter a new number from 1 to 9: "
-        move = raw_input("Your move: ")
+        move = raw_input("{}'s ({}) move: ".format(current_player,symbol))
     entered.append(move)
     cells['key_'+move]=" "+symbol+" " #assigns move to board
     
     
+#----------------------------------------------------------
+#   This last function is dependent on all above functions.
+#----------------------------------------------------------
 
-#--------------------------------------------
-#   CODE TO RUN THE GAME
-#--------------------------------------------
-
-#these global variables need to be set
-play_count = 0
-entered = []
-cells   = {'key_1':'[1]','key_2':'[2]','key_3':'[3]',
-          'key_4':'[4]','key_5':'[5]','key_6':'[6]',
-          'key_7':'[7]','key_8':'[8]','key_9':'[9]'}
-
-#this function gets the player's names and gives instructions to play
-get_players()
-
-#this while loop runs the game
-while win_check() == False:
-    if len(entered) >= 9:
-        break
+def play_game():
+    #these global variables need to be set
+    global play_count
+    global entered
+    global cells
+    play_count = 0
+    entered = []
+    cells   = {'key_1':'[1]','key_2':'[2]','key_3':'[3]',
+              'key_4':'[4]','key_5':'[5]','key_6':'[6]',
+              'key_7':'[7]','key_8':'[8]','key_9':'[9]'}
+    
+    #this function gets the player's names and gives instructions to play
+    get_players()
+    
+    #this while loop runs the game
+    while win_check() == False:
+        if len(entered) >= 9:
+            break
+        else:
+            print_board()
+            print "Play count is: {}\n".format(play_count)
+            play_count += 1
+            make_move()
+    
+    #this block of code executes when game has ended
+    print "\n"
+    print_board()
+    if win_check() == False:
+        print "\n Draw game :)"
+    elif play_count % 2 == 1:
+        print "\n"+"{} wins! ".format(player_1)*3
     else:
-        print_board()
-        print "Play count is: {}\n".format(play_count)
-        play_count += 1
-        make_move()
+        print "\n"+"{} wins! ".format(player_2)*3
+    raw_input()
 
-#this block of code executes when game has ended
-print "\n"
-print_board()
-if win_check() == False:
-    print "\n Draw game :)"
-elif play_count % 2 == 1:
-    print "\n"+"{} wins! ".format(player_1)*3
-else:
-    print "\n"+"{} wins! ".format(player_2)*3
-raw_input()
+
+#------------------
+#   PLAY GAME!
+#------------------
+
+play_game()
